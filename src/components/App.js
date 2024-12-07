d561601630f4c1cc2a392fa59f291994435f7eee
Super Dev <superdev1031@gmail.com>
2024-12-07 15:40:20 -0500

    invoking callbacks in children

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import SearchInput from "./SearchInput";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "30px" }}>
-      <SearchInput />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "30px" }}>
+        <SearchInput onSearchSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -9,7 +9,7 @@ class SearchInput extends React.Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state.entry);
+    this.props.onSearchSubmit(this.state.entry);
   };
 
   render() {
