13e44d1632f09b1b853dad2857f91a6b35959423
Super Dev <superdev1031@gmail.com>
2024-12-14 11:14:17 -0500

    show image list

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,28 @@
 import React from "react";
 import SearchInput from "./SearchInput";
+import ImageList from "./ImageList";
 import axios from "axios";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+
   async onSearchSubmit(entry) {
     const response = await axios.get(
       `https://pixabay.com/api/?key=47513703-0496c1f53313eee209eceb317&q=${entry}&image_type=photo`
     );
     console.log(response.data.hits);
+    this.setState({ images: response.data.hits });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "30px" }}>
         <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
