2bc669d8423244370a4ee51ff192ec90790647c7
Super Dev <superdev1031@gmail.com>
2024-12-14 10:26:53 -0500

    fetch data using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import SearchInput from "./SearchInput";
+import axios from "axios";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  async onSearchSubmit(entry) {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=47513703-0496c1f53313eee209eceb317&q=${entry}&image_type=photo`
+    );
+    console.log(response.data.hits);
   }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "30px" }}>
